676e4160cd3622590c91eba355c678f15cb715f2
POPCHST <dusita.siit@gmail.com>
2025-06-06 19:02:52 +0700

    pop แก้ไข

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -15,6 +15,11 @@ export default function AboutPage() {
 
       <StackedImagesLayout
         titleColor={themeColor1}
+        images={[
+          "https://res.cloudinary.com/dyg6r8pec/image/upload/v1749194133/%E0%B9%80%E0%B8%81%E0%B8%B5%E0%B9%88%E0%B8%A2%E0%B8%A7%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B9%80%E0%B8%AD%E0%B9%80%E0%B8%A7%E0%B8%AD%E0%B8%A3%E0%B9%8C-03_0_wrvobb.jpg",
+          "https://res.cloudinary.com/dyg6r8pec/image/upload/v1749194135/%E0%B9%80%E0%B8%81%E0%B8%B5%E0%B9%88%E0%B8%A2%E0%B8%A7%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B9%80%E0%B8%AD%E0%B9%80%E0%B8%A7%E0%B8%AD%E0%B8%A3%E0%B9%8C-04_0_o9jg9x.jpg",
+        ]}
+        imageSize="w-[500px] h-[400px]"
         content={``}
         subContent={`ดั่งคำขวัญที่ว่า\n \n “คุณภาพมาตราฐาน บริการฉับไว ใส่ใจลูกค้า\nพัฒนาอย่างต่อเนื่อง และห่วงใยสิ่งแวดล้อม!”`}
         subContentFontSize="text-1xl"
@@ -28,12 +33,28 @@ export default function AboutPage() {
         contentAlign="left"
         content={`1. องค์กรดำเนินธุรกิจให้สอดคล้องกับข้อกฎหมาย\n2. ส่งเสริมให้พนักงานตระหนักด้านสิ่งแวดล้อมให้สอดคล้องกับข้อปฎิบัติของบริษัท\n 3. ควบคุมในกระบวนการด้านการปฎิบัติงานด้านสิ่งแวดล้อม\n 4. ป้องกัน เฝ้าติดตาม และตรวจวัดเรื่องมลภาวะด้านสิ่งแวดล้อม`}
         images={[
-          "https://media.istockphoto.com/id/2195989736/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B9%80%E0%B8%81%E0%B8%A9%E0%B8%95%E0%B8%A3%E0%B8%81%E0%B8%A3%E0%B8%A1%E0%B8%B5%E0%B8%AA%E0%B9%88%E0%B8%A7%E0%B8%99%E0%B8%A3%E0%B9%88%E0%B8%A7%E0%B8%A1%E0%B9%83%E0%B8%99%E0%B8%87%E0%B8%B2%E0%B8%99%E0%B9%80%E0%B8%81%E0%B8%A9%E0%B8%95%E0%B8%A3%E0%B9%82%E0%B8%94%E0%B8%A2%E0%B9%80%E0%B8%89%E0%B8%9E%E0%B8%B2%E0%B8%B0%E0%B9%83%E0%B8%99%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%95%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%84%E0%B9%88%E0%B8%B2%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%9E%E0%B8%B2%E0%B8%B0%E0%B8%9B%E0%B8%A5%E0%B8%B9%E0%B8%81%E0%B9%80%E0%B8%AB%E0%B9%87%E0%B8%94%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87%E0%B9%81%E0%B8%88%E0%B9%89%E0%B8%87%E0%B8%95%E0%B8%A3%E0%B8%A7%E0%B8%88%E0%B8%AA%E0%B8%AD%E0%B8%9A%E0%B8%A0%E0%B8%B2%E0%B8%8A%E0%B8%99%E0%B8%B0%E0%B8%9C%E0%B8%B1%E0%B8%81.jpg?s=1024x1024&w=is&k=20&c=SPX_X6E1_LpkuZiO50896tydTkdChIMR6dw9yJ4U5r4=",
+          "https://res.cloudinary.com/dyg6r8pec/image/upload/v1749194146/%E0%B9%80%E0%B8%81%E0%B8%B5%E0%B9%88%E0%B8%A2%E0%B8%A7%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B9%80%E0%B8%AD%E0%B9%80%E0%B8%A7%E0%B8%AD%E0%B8%A3%E0%B9%8C-05_0_fkjdat.jpg",
         ]}
-        imageSize="w-25 h-25"
+        imageSize="w-[1000px] h-[400px]"
         subContentFontSize="text-1xl"
         subContentColor={themeColor1}
         subContentBackground="border-only"
+      />
+      <p>
+        บริษัทเอเวอร์สปริง อโกรเคม จำกัด ก่อตั้งในปี 2544 เป็นผู้นำเข้า
+        ผลิตแบ่่งบรรจุ และจัดจำหน่ายสินค้าเคมีเกษตรทั้งสารกำจัดวัชพืช
+        สารกำจัดแมลง สารป้องกันกำจัดโรคพืช และฮอร์โมนอาหารเสริม
+      </p>
+
+      <StackedImagesLayout
+        title="คุณภาพบริษัท"
+        content={`บริษัทเอเวอร์สปริง อโกรเคม จำกัด ผู้นำเข้าผลิตแบ่งบรรจุ \nสินค้าเคมีเกษตรที่ได้รับการรับรองมาตราฐาน ISO 9001 และ 14001`}
+        images={[
+          "https://www.arenasolutions.com/wp-content/uploads/what-is-iso-9001-compliance.png",
+          "https://www.citrecolor.it/wp-content/uploads/2021/02/iso-14001.png",
+        ]}
+        titleColor={themeColor1}
+        imagePosition="middle"
       />
     </div>
   );
