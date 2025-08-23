0724f9bba29df5878e2c911a4cd244a8229aed81
PGM-NB-05\PGM-NB-05 <chirdsakdb@gmail.com>
2025-08-23 15:48:49 +0700

    tailwindcss

--- src/app/components/layout-pattern/contact-maps.tsx
+++ src/app/components/layout-pattern/contact-maps.tsx
@@ -5,7 +5,7 @@ export default function ContactMaps(props: { text?: string }) {
         <div className="flex justify-center items-center w-[100%] h-72 rounded-lg overflow-hidden">
             <iframe
                 src={props?.text}
-                className="w-full md:w-[50%] h-[100%] border border-slate-600 rounded-xl shadow-lg"
+                className="w-full md:w-[70%] h-[100%] border border-slate-600 rounded-xl shadow-lg"
                 style={{ border: 0 }}
                 allowFullScreen={true}
                 loading="lazy"
