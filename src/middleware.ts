38d1574cf907216ffd9b5c68ae28756e4568cbbc
big <bigname1101@gmail.com>
2025-09-06 23:19:43 +0700

    feat(auth): add auth js
    - add auth js
    - update middleware for support auth
    - update tsconfig

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,29 @@
+import NextAuth, { NextAuthRequest } from 'next-auth';
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
- 
+import authConfig from './app/configs/auth.config';
+
+const { auth } = NextAuth(authConfig);
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export default auth(async function middleware(request: NextRequest & NextAuthRequest) {
   const pathnames = request.nextUrl.pathname.split('/');
-  
+
   switch (pathnames[1]) {
     case 'th':
     case 'en':
       return NextResponse.next();
+    case 'admin':
+      if (pathnames.length > 2 && !request?.auth) {
+        return NextResponse.redirect(new URL('/admin', request.url));
+      }
+
+      return NextResponse.next();
     default:
-      return NextResponse.redirect(new URL('/th', request.url))
+      return NextResponse.redirect(new URL('/th', request.url));
   }
-}
- 
+})
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/', '/:lang'],
+  matcher: ['/', '/:lang', '/admin/:path*'],
 }
